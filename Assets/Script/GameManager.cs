760c715b732c0a1107907d049957098f8a7c63a3
Asif Saheer <saheerasif50@gmail.com>
2024-02-28 01:37:14 +0530

    Add force to the player

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     public GameObject startingPage;
     public GameObject gameOverPage;
     public GameObject runningPage;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +22,19 @@ public class GameManager : MonoBehaviour
     }
     public void PlayButton() 
     {
+        player.SetActive(true);
         startingPage .SetActive(false);
         runningPage .SetActive(true);
     }
     public void RetryButton()
     {
+        player.SetActive(true);
         gameOverPage.SetActive(false);
         runningPage.SetActive(true);
     }
     public void HomeButton()
     {
+        player.SetActive(false);
         gameOverPage .SetActive(false);
         startingPage .SetActive(true);
     }
--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public int jumbingForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb= GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumbingForce;
+        }
     }
 }
