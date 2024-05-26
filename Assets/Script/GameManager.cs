f8fd39d4d3d12ed2334081af9cfd9f2a5cb98041
Asif Saheer <saheerasif50@gmail.com>
2024-05-26 12:02:38 +0530

    scene and pipe
    nothing important

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverPage;
     public GameObject runningPage;
     public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
