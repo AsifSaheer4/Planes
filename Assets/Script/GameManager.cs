fe605099eaea104042fc895bda43643dfcb2bd50
Asif Saheer <saheerasif50@gmail.com>
2024-05-27 01:09:14 +0530

    Spawning building  completed

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,11 +9,16 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverPage;
     public GameObject runningPage;
     public GameObject player;
+    public GameObject building;
+
+    float buildingPosY;
+    float  buildingSpawnigTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        buildingSpawnigTime = Random.Range(0.5f, 2);
+        InvokeRepeating("spawningBuilding", 2f, buildingSpawnigTime);
     }
 
     // Update is called once per frame
@@ -38,6 +43,14 @@ public class GameManager : MonoBehaviour
         player.SetActive(false);
         gameOverPage .SetActive(false);
         startingPage .SetActive(true);
+    }
+    void spawningBuilding()
+    {
+        buildingPosY = Random.Range(-3, -10);
+
+        buildingSpawnigTime = Random.Range(0.5f, 2);
+
+        Instantiate(building, new Vector2(30, buildingPosY),Quaternion.identity);
     }
     
 }
